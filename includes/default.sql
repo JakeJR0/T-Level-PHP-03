893601bc3b674e249c4d454dbac1769ee5478434
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-10-07 00:17:18 +0100

    Updating default.sql

--- includes/default.sql
+++ includes/default.sql
@@ -9,6 +9,13 @@ CREATE TABLE IF NOT EXISTS course_levels(
     FOREIGN KEY (course_id) REFERENCES courses(ID)
 );
 
+CREATE TABLE feedback(
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT NOT NULL,
+    feedback VARCHAR(255) NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(ID)
+);
+
 INSERT INTO courses(course_name) VALUES('DPDD'), ('DBS'), ('DSS');
 INSERT INTO course_levels(course_id, course_level) 
 VALUES
@@ -21,3 +28,4 @@ VALUES
 (1, 'Advanced'),
 (2, 'Advanced'),
 (3, 'Advanced');
+
